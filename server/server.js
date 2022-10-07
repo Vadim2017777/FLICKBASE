562e388741565c75d0ef1c7aa3597dbfa6280d90
V <svanovivan@gmail.com>
2022-10-07 17:25:43 +0300

    Add models

--- server/server.js
+++ server/server.js
@@ -5,7 +5,18 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const MongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUri);
+
+app.use(bodyParser.json());
+
+app.use(xss());
+
+app.use(mongoSanitize());
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
