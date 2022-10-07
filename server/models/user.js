6e464425d5df0c3125191c8f4cadbc4d0980b5a6
V <svanovivan@gmail.com>
2022-10-07 18:05:18 +0300

    Add models

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,55 @@
-const mongoose = require("");
+const mongoose = require("mongoose");
+const validator = require("validator");
+require("dotenv").config();
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validator(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin "],
+  },
+  firstname: {
+    type: String,
+    maxLength: 100,
+    trim: true,
+  },
+
+  lastname: {
+    type: String,
+    maxLength: 100,
+    trim: true,
+  },
+
+  age: {
+    type: Number,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
