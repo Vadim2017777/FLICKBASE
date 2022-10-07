b270023d69646cc8e11d4611881bef0f26df13f7
V <svanovivan@gmail.com>
2022-10-07 12:16:32 +0300

    Add mongo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const MongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
